37fa963e23e1e697a4670f8605d9020e7c25fa7b
jdev91 <devlin14@up.edu>
2013-07-28 20:17:51 -0700

    Board DONE

--- src/game/Main.java
+++ src/game/Main.java
@@ -8,27 +8,50 @@ public class Main
      */
     public static void main(String[] args)
     {        
-        int[][] board = new int[18][26];
+        char[][] board = new char[18][26];
         
+        boolean toggle = true;
         //rows
         for (int i = 0; i < board.length; i++) {
             //cols
+        	toggle = !toggle;
             for(int j = 0; j < board[i].length; j++) {
                 
                 //upper triangle
-                if( (i > 14-j) && (i < 5) && (i > j-12) ) {
-                    board[i][j] = 1;
+                if( (i < 5) && (i >= 14-j) && (i >= j-12) ) {
+                    board[i][j] = '1';
                 }
                 //bottom triangle
-                else if( (i > 13) && (i < j+4) && (i < 30-j) ) {
-                    board[i][j] = 2;
+                else if( (i > 13) && (i <= j+4) && (i <= 30-j) ) {
+                    board[i][j] = '2';
+                }
+                //top left triangle
+                else if ( (i > 4) && (i <= j+4) && (i <= 12-j) ) {
+                    board[i][j] = '3';
+                }
+                //top right triangle
+                else if ( (i > 4) && (i <= j-14) && (i <= 30-j) ) {
+                    board[i][j] = '4';
                 }
                 //bottom left triangle
-                else if( (i < 14) && (i > j+6) && (i > 11-j) ) {
-                    board[i][j] = 3;
-                } else {
-                    board[i][j] = 0;
+                else if( (i < 14) && (i >= j+6) && (i >= 14-j) ) {
+                    board[i][j] = '5';
                 }
+                //bottom right triangle
+                else if( (i < 14) && (i >= j-12) && (i >= 32-j) ) {
+                    board[i][j] = '6';
+                }                
+                //middle hexagon
+                else if( (i > 4) && (i < 14) && (j > 4) && (j < 22)) {
+                    board[i][j] = '7';
+                }
+                else {
+                    board[i][j] = ' ';
+                }
+                if(toggle){
+                	board[i][j] = ' ';
+                }
+                toggle = !toggle;
             }
         }       
 
